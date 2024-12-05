05c01cb80458771539009c90c1513f21e21a86de
ZhenyaRom <evladro2020@gmail.com>
2024-12-05 18:29:06 +0500

    04-12-2024lesson89

--- UrbanDj/UrbanDj/settings.py
+++ UrbanDj/UrbanDj/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example1',
+    'example2',
+    'django.core.paginator',
 ]
 
 MIDDLEWARE = [
